ae9a0b6e7361f72d976ba539ecaae0c6e44ec0a6
kangsunku <134983385+kangsunku@users.noreply.github.com>
2024-05-23 16:31:35 +0900

    Update nfs_server_init.sh - ip change

--- vagrant/nfs_server/nfs_server_init.sh
+++ vagrant/nfs_server/nfs_server_init.sh
@@ -7,7 +7,7 @@ if [[ ! -d /nfsdir ]]; then
     mkdir /nfsdir
 fi
 
-echo "/nfsdir   192.168.29.0/24(rw,sync,no_root_squash)" >> /etc/exports 
+echo "/nfsdir   192.168.50.0/24(rw,sync,no_root_squash)" >> /etc/exports 
 if [[ $(systemctl is-enabled nfs-kernel-server) -eq "disabled" ]]; then
     systemctl enable nfs-kernel-server
 fi
